234a5581e313c297885ad0f1a74bb726027ef50d
treyyoder <jyoder@dtec.com>
2015-09-01 13:32:32 -0400

    Renaming package

--- package.js
+++ package.js
@@ -1,5 +1,5 @@
 Package.describe({
-  name: 'treyyoder:mapper',
+  name: 'treyyoder:grits-net-mapper',
   version: '0.0.1',
   summary: '',
   git: '',  
@@ -9,13 +9,13 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom('1.1.0.3');  
   api.use('coffeescript');
-  api.use('fuatsengul:leaflet', 'client'); 
+  api.use('fuatsengul:leaflet', 'client');
   api.addFiles('leafnav.js', ['client']);
-  api.addFiles('mapper.coffee', ['client']);
+  api.addFiles('grits-net-mapper.coffee', ['client']);
 });
 
 Package.onTest(function(api) {
   api.use('tinytest');
-  api.use('treyyoder:mapper');
-  api.addFiles('mapper-tests.js');
+  api.use('treyyoder:grits-net-mapper');
+  api.addFiles('grits-net-mapper-tests.js');
 });
